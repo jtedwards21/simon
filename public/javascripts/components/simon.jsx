e87fa1875adac4ffeb2ee5f72f0b92a045f7c632
James Ahlm <14782828428@qq.com>
2016-11-28 11:10:28 +0800

    added code to start a game

--- public/javascripts/components/simon.jsx
+++ public/javascripts/components/simon.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-var onChange = function(gameState) {
+var onChange = function(gameState, userState) {
 　　ReactDOM.render(<Simon gameState={gameState} userState={userState}/>, document.getElementById("content"));
 }
 
@@ -15,11 +15,25 @@ export default class Simon extends React.Component {
       userState: []
     };
   }
-  addColorClick() {
-    //add the user click to state?
-    //slice the gameState array
-    //compare the user and the gameState
-    //if wrong blink red and dump the game
+  addColorClick(e) {
+    var s = this.props.userState;
+    s.push(e.target.id)
+    var l = s.length;
+    var gameS = this.props.gameState.slice(0,l);
+    var curr = l - 1;
+    var same = s[curr] == gameS[curr];
+    if(same == false){
+	//Blink red and dump
+    }
+    else{
+      if(l == this.props.gameState.length){
+	//Blink green and get more gameState pieces
+        //Show new gamestate before rerender
+      }
+      else{
+	//continue current state
+      }
+    }
     //check lengths
     //if not the same allow to continue to click
     //if the same blink green and finish
@@ -27,6 +41,21 @@ export default class Simon extends React.Component {
   lightButtons() {
     
     //Light up the buttons in the Simon Game
+  }
+  startGame(){
+    //Get a new GameState
+    var b = this.randomInt();
+    var newState = this.buttons[b];
+    //Show the gameState
+    var e = document.getElementById("#" + newState);
+    var originalColor = e.style.backgroundColor;
+    e.style.backgroundColor = "white";
+    //delay for a moment
+    e.style.backgroundColor = originalColor;
+    //onChange
+    var gs = this.props.gameState;
+    gs.push(newState);
+    onChange(gs, this.props.userState);
   }
   randomInt() {
     var min = Math.ceil(0);
@@ -38,18 +67,22 @@ export default class Simon extends React.Component {
       <div className="box">
 	<div className="buttons">
           <div className="top-row">
-	    <div id="button-one" className="button left-col">
+	    <div id="button-one" onClick={this.addColorClick.bind(this)} className="button left-col">
             </div>
-            <div id="button-two" className="button right-col">
+            <div id="button-two" onClick={this.addColorClick.bind(this)} className="button right-col">
             </div>
 	  </div>
           <div className="bottom-row">
-            <div id="button-three" className="button left-col">
+            <div id="button-three" onClick={this.addColorClick.bind(this)} className="button left-col">
             </div>
-            <div id="button-four" className="button right-col">
+            <div id="button-four" onClick={this.addColorClick.bind(this)} className="button right-col">
             </div>
 	  </div>
 	</div>
+	<div className="display-container">
+	  <span className="display"></span>
+	</div>
+        <button　onClick={this.startGame.bind(this)}>Start</button>
       </div>
     )
   }
